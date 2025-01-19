4fc9a81176bd98b1ef423385d5793948af683fe5
CClady87 <rtoczydlowski28@gmail.com>
2025-01-19 00:40:05 -0500

    Make these changes

--- my_first_script.py
+++ my_first_script.py
@@ -1,36 +1,54 @@
-print ("Hello, World!")
+#print ("Hello, World!")
 #Apple will be known as food 
-cost = 2.75
+#cost = 2.75
 #This is how much an apple cost
-print (5>3)
-print ( 5 > 3 )
-print ( True and False)
-print ( True or False)
-print (4 * 5)
-print (18 / 2 )
-food_a = "apple"
-food_b = "pie"
-combined_food = food_a + "" + food_b
-print (combined_food)
-favorite_food = "pizza"
-my_favorite_food = "Your favorite food is" + " " + favorite_food + "!"
-print ( my_favorite_food)
-food_output = f"Your favorite food is {favorite_food}!"
-store = "walmart"
-print (store.upper())
-message = "I shop at the store"
-print (message.replace("the store", "Walmart"))
+#print (5>3)
+#print ( 5 > 3 )
+#print ( True and False)
+#print ( True or False)
+#print (4 * 5)
+#print (18 / 2 )
+#food_a = "apple"
+#food_b = "pie"
+#combined_food = food_a + "" + food_b
+#print (combined_food)
+#favorite_food = "pizza"
+#my_favorite_food = "Your favorite food is" + " " + favorite_food + "!"
+#print ( my_favorite_food)
+#food_output = f"Your favorite food is {favorite_food}!"
+#store = "walmart"
+#print (store.upper())
+#message = "I shop at the store"
+#print (message.replace("the store", "Walmart"))
 
-favorite_food = input("What is your favorite food?")
-print(f"Wow, I love to eat {favorite_food}")
+#favorite_food = input("What is your favorite food?")
+#print(f"Wow, I love to eat {favorite_food}")
 
-num = int("10")
-print (num)
-pi = 3.14
-print (int(pi))
-cost = "7.50"
-print (float(cost))
-type_error_result = "7.50" + "7.50"
-favorite_color=input("What is your favorite color?")
-print(f"That shade of {favorite_color} is my favorite!")
+#num = int("10")
+#print (num)
+#pi = 3.14
+#print (int(pi))
+#cost = "7.50"
+#print (float(cost))
+#type_error_result = "7.50" + "7.50"
+#favorite_color=input("What is your favorite color?")
+#print(f"That shade of {favorite_color} is my favorite!")
+#milk = 4
+#bread = 2.50
+#cheese = 3.75
+#print (milk + bread + cheese)
+#total_cost = "10.25"
+#cost_output = f"Your total is:{(float)total_cost}"
+#total = "Your total is" + " " + "$" + total_cost
+#print(total)
+#favorite_grocery_store = input("What is your favorite grocery store?")
+#finding out what your favorite store is
+#print(f"Welcome to {favorite_grocery_store}! We are happy to see you back! Enjoy shopping with us today!")
+#exploiting the information against you
 
+milk = 3.0
+bread = 2.5
+total = milk + bread
+#print(total)
+total_cost = "5.5"
+print("The total cost is: $" + total_cost)
